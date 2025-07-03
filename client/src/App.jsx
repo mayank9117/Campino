eb5a3945e72dbe30613f45f1e5031a53a9d62c63
Ajay Kumar Singh <singhajay3717@gmail.com>
2025-07-03 09:56:12 +0530

    admin section partially done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
-import {Route,Routes} from 'react-router-dom';
-import Home  from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 import Blog from './pages/Blog';
-import { dashboard_data } from './assets/assets';
 import DashBoard from './pages/admin/DashBoard';
-import AddBlog from './pages/admin/AddBlog'
+import AddBlog from './pages/admin/AddBlog';
 import ListBlog from './pages/admin/ListBlog';
 import Comments from './pages/admin/Comments';
 import Layout from './pages/admin/Layout';
+import Login from './components/admin/Login';
 
 const App = () => {
+  const isAuthenticated = true; // replace this with real auth check later
+
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home />} />
         <Route path='/blog/:id' element={<Blog />} />
-        <Route path='admin' element={<Layout/>}>
-
-         <Route index element={<DashBoard/>}/>
-         <Route path='addBlog' element={<AddBlog/>}/>
-         <Route path='listBlog' element={<ListBlog/>}/>
-         <Route path='comments' element={<Comments/>}/>
+        <Route path='admin' element={isAuthenticated ? <Layout /> : <Login />}>
+          <Route index element={<DashBoard />} />
+          <Route path='addBlog' element={<AddBlog />} />
+          <Route path='listBlog' element={<ListBlog />} />
+          <Route path='comments' element={<Comments />} />
         </Route>
       </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
